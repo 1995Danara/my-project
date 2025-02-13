d375a9726de2093f83cdbc0721350b73699ff897
Eugene Allenov <imr@inbox.ru>
2025-02-13 21:58:39 +0300

    eth fix

--- src/components/Balance/index.tsx
+++ src/components/Balance/index.tsx
@@ -1,12 +1,13 @@
 "use client"
 import React from "react"
 import { useAccount, useBalance } from "wagmi"
-
+import { useIsWrongNetwork } from "../hooks/useWrongNetwork"
 import { TARGET_NETWORK_ID } from "../../../networkConfig"
 import { Box, Typography } from "@mui/material"
 import { formatUnits } from "viem"
 export const Balance = () => {
   const { address, isConnected } = useAccount()
+  const isWrongNetwork = useIsWrongNetwork()
   const { data: balance } = useBalance({
     address,
     chainId: TARGET_NETWORK_ID,
@@ -15,26 +16,29 @@ export const Balance = () => {
   const formattedBalance = balance
     ? `${Number(formatUnits(balance.value, 18)).toFixed(4)} ETH`
     : "N/A"
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "10px",
-      }}
-    >
-      <Box>
-        {isConnected ? (
-          <Typography variant="body1" sx={{ marginRight: "10px" }}>
-            Balance: {formattedBalance}
-          </Typography>
-        ) : (
-          <Typography variant="body1" sx={{ marginRight: "10px" }}>
-            Balance: N/A
-          </Typography>
-        )}
+
+  if (isConnected && !isWrongNetwork) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "10px",
+        }}
+      >
+        <Box>
+          {isConnected ? (
+            <Typography variant="body1" sx={{ marginRight: "10px" }}>
+              Balance: {formattedBalance}
+            </Typography>
+          ) : (
+            <Typography variant="body1" sx={{ marginRight: "10px" }}>
+              Balance: N/A
+            </Typography>
+          )}
+        </Box>
       </Box>
-    </Box>
-  )
+    )
+  }
 }
--- src/components/TokenBalance/index.tsx
+++ src/components/TokenBalance/index.tsx
@@ -2,13 +2,18 @@
 import { useReadContract, useAccount } from "wagmi"
 import { ContractConfig } from "../ContractConfig "
 import { Box } from "@mui/material"
-
+import { useIsWrongNetwork } from "../hooks/useWrongNetwork"
 export const TokenBalance = () => {
   const { isConnected } = useAccount()
+  const isWrongNetwork = useIsWrongNetwork()
   const { data: balance } = useReadContract({
     ...ContractConfig,
     functionName: "balanceOf",
     args: ["0xCaD8610808Ba58c03f21F9Ecd3b12D5A26D58760"],
   })
-  if (isConnected) return <Box>Balance: {balance?.toString()}</Box>
+  if (isConnected && !isWrongNetwork) {
+    {
+      return <Box>Balance: {balance?.toString()}</Box>
+    }
+  }
 }
