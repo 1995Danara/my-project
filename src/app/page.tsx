fdbd98deaf3fe881ba4e9820d62dd78cacb0c051
Eugene Allenov <imr@inbox.ru>
2025-03-10 11:27:48 +0300

    dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 "use client"
-import { Box, Typography } from "@mui/material"
+import { Box, Typography, Button } from "@mui/material"
+import { useState } from "react"
 
 import { Balance } from "@components/Balance"
 import { TokenBalance } from "@components/TokenBalance"
 import { Header } from "@components/Header"
 import { MetaMaskTokenButton } from "@components/MetaMaskTokenButton "
 import { TokenTransfer } from "@components/TokenTransfer"
+import { WalletDialog } from "@components/WalletDialog"
+import { useWalletConnect } from "@hooks/useWalletConnect"
 
 export function HomePage() {
+  const { address } = useWalletConnect()
+
+  const [openDialog, setOpenDialog] = useState(false)
+
+  const handleOpenDialog = () => setOpenDialog(true)
+  const handleCloseDialog = () => setOpenDialog(false)
+
   return (
     <Box
       sx={{
@@ -19,27 +29,54 @@ export function HomePage() {
       }}
     >
       <Header />
-      <TokenBalance />
-      <Balance />
-      <Box
-        sx={{
-          backgroundColor: "white",
-          padding: 4,
-          borderRadius: 2,
-          boxShadow: 6,
-          display: "flex",
-          flexDirection: "column",
-          textAlign: "center",
-          minWidth: 600,
-          justifyContent: "center",
-          minHeight: 400,
-          gap: 4,
-        }}
-      >
-        <Typography variant="h6">Transfer of tokens</Typography>
-        <TokenTransfer />
-        <MetaMaskTokenButton />
-      </Box>
+      {!address ? (
+        <Box
+          sx={{
+            backgroundColor: "white",
+            borderRadius: 2,
+            boxShadow: 6,
+            minWidth: 600,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            minHeight: 400,
+          }}
+        >
+          <Button
+            size="large"
+            variant="contained"
+            color="secondary"
+            onClick={handleOpenDialog}
+          >
+            Connect Wallet
+          </Button>
+        </Box>
+      ) : (
+        <>
+          <TokenBalance />
+          <Balance />
+          <Box
+            sx={{
+              backgroundColor: "white",
+              padding: 4,
+              borderRadius: 2,
+              boxShadow: 6,
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              minWidth: 600,
+              justifyContent: "center",
+              minHeight: 400,
+              gap: 4,
+            }}
+          >
+            <Typography variant="h6">Transfer of tokens</Typography>
+            <TokenTransfer />
+            <MetaMaskTokenButton />
+          </Box>
+        </>
+      )}
+      <WalletDialog open={openDialog} onClose={handleCloseDialog} />
     </Box>
   )
 }
